3ceadf73cca0bbf77720d6a7402f6cc5566a9703
Vic Ku - TW (Production) <VicK@supermicro.com>
2020-08-24 15:24:22 +0800

    add getCurrentMonth

---
+++ src/Data/createStore.js
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
+import reducer from "./appState";
+
+
+const initialState = {};
+const logger = createLogger();
+
+const rootReducer = combineReducers({
+  app: reducer,
+
+});
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(logger)
+  // other store enhancers if any
+);
+
+// const store = createStore(rootReducer, initialState, enhancer); develop only
+const store = createStore(rootReducer, initialState,enhancer);
+
+export default store;
--- src/ParsingData/ParsingHelpFunction.js
+++ src/ParsingData/ParsingHelpFunction.js
@@ -126,3 +126,22 @@ export function outputDate(date) {
   export function pickUpStationByCMVendorForPie(cm){
     return VENDOR_CODE_WITH_STATION_FUNCTION[cm]
   }
+
+  export function getCurrentMonth(){
+    const d = new Date();
+    const month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    return month[d.getMonth()].substr(0,3);
+  }
+  
