578e72f0909b54da4036e389c52604213aa56301
Vic Ku - TW (Production) <VicK@supermicro.com>
2020-10-26 16:15:58 +0800

    fixed

--- src/Components/RepairTabel.js
+++ src/Components/RepairTabel.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Table } from 'react-bootstrap'
+import { outputDate } from '../ParsingData/ParsingHelpFunction'
 
 export default function RepairTable({ data }) {
-    return (
+    console.log('repair table data', data)
+    return data.length ? (
         <Table
             striped
             bordered
@@ -22,18 +24,24 @@ export default function RepairTable({ data }) {
                 </tr>
             </thead>
             <tbody>
-                {data.map((item, i) => (
-                    <tr key={i}>
-                        <td>{item.Date}</td>
-                        <td>{item.Model}</td>
-                        <td>{item.Type}</td>
-                        <td>{item.Error_Description}</td>
-                        <td>{item.Reason}</td>
-                        <td>{item.item}</td>
-                        <td>{item.Cust_PN}</td>
-                    </tr>
-                ))}
+                {data && data.length
+                    ? data.map((item, i) => (
+                          <tr key={i}>
+                              <td>{item.Date ? outputDate(item.Date) : ''}</td>
+                              <td>{item.Model ? item.Model : ''}</td>
+                              <td>{item.Type ? item.Type : ''}</td>
+                              <td>
+                                  {item.Error_Description
+                                      ? item.Error_Description
+                                      : ''}
+                              </td>
+                              <td>{item.Reason ? item.Reason : ''}</td>
+                              <td>{item.item ? item.item : ''}</td>
+                              <td>{item.Cust_PN ? item.Cust_PN : ''}</td>
+                          </tr>
+                      ))
+                    : null}
             </tbody>
         </Table>
-    )
+    ) : null
 }
